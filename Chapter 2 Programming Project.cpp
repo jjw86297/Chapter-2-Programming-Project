1ecefd61b84c03f157c358587f2465e9e6d65f1b
jjw86297 <jjw86297@email.vccs.edu>
2024-09-19 21:56:23 -0400

    Program Demo

--- Chapter 2 Programming Project.cpp
+++ Chapter 2 Programming Project.cpp
@@ -4,19 +4,23 @@
 //Distance_per_Tank
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double gas_tank = 20;
+
+    double town = 23.5;
+    double highway = 28.9;
+
+    string method;
+
+    cout << "Are you travelling in town or on the highway? Type one of the two in all lowercase. ";
+    cin >> method;
+
+    double distance = gas_tank * method;
+
+    cout << "You will be able to drive " << distance << " miles in that case!";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
